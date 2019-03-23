30a947f7e5dd8f6e3ed40d4d18820ad8a3d87fe1
Guzooo <guzooo-guzooo@o2.pl>
2019-03-23 09:13:37 +0100

    0.1.1
    Dodanie wyglądu dla Activity zarządzania graczami, oraz dla widoku pojedynczego gracza
    Drobne modyfikacje w stylu aplikacji

--- java/pl/Guzooo/PilkarskiPrzybornik/MainActivity.java
+++ java/pl/Guzooo/PilkarskiPrzybornik/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.Guzooo.PilkarskiPrzybornik;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void ClickPlayersControl(View v){
+        Intent intent = new Intent(this, PlayersActivity.class);
+        startActivity(intent);
     }
 }
